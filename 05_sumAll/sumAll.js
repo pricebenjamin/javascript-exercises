d370751a0f0e2577c26e46bdb4397b6ac9a8a444
Ben Price <price.benjamindaniel@gmail.com>
2024-02-16 14:43:54 -0800

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a == b) return a; // not covered by tests
 
+    if (typeof a != "number" || typeof b != "number") {
+        return "ERROR";
+    }
+
+    // tests dictate return ERROR with negative numbers
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    // ensure a < b so that the following loop works
+    if (b < a) {
+        [a, b] = [b, a]
+    }
+
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
