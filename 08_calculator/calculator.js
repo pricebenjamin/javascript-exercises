41bdb452e35fb9fe6481068db1e183ef02f5b804
Ben Price <price.benjamindaniel@gmail.com>
2024-02-16 15:27:21 -0800

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n == 0) return 1;
+  let product = 1;
+  for (let m = 2; m <=n; m++) {
+    product *= m;
+  }
+  return product;
 };
 
 // Do not edit below this line
