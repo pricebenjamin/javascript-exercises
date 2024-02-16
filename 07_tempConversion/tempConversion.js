79b39b35c3b521f8167d5fa7e272ee7527d5fba0
Ben Price <price.benjamindaniel@gmail.com>
2024-02-16 15:14:40 -0800

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp_in_degF) {
+  return roundToOneDigit((temp_in_degF - 32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp_in_degC) {
+  return roundToOneDigit((temp_in_degC * (9 / 5)) + 32);
 };
+
+function roundToOneDigit(n) {
+  return Number(n.toFixed(1));
+}
 
 // Do not edit below this line
 module.exports = {
