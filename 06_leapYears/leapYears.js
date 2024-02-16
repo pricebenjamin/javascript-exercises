691cb73ce6d2679f66436dfa68de3d4b3691acae
Ben Price <price.benjamindaniel@gmail.com>
2024-02-16 15:04:58 -0800

    Provide an explanation regarding the order of checks

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,9 @@
 const leapYears = function(year) {
+
+    // Note: The order of these cases matters.
+    // Since any number divisible by 400 is necessarily divisible
+    // by 100 and by 4, we must check cases in the following order.
+
     if (year % 400 == 0) {
         return true;
     } else if (year % 100 == 0) {
