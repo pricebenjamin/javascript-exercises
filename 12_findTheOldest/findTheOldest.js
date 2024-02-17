5da9ed0978c01e6f13ff07cfc8a631c3806216af
Ben Price <price.benjamindaniel@gmail.com>
2024-02-16 16:32:46 -0800

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort(ascendingAge).at(-1);
 };
+
+function ascendingAge(person_a, person_b) {
+    return computeAge(person_a) - computeAge(person_b);
+}
+
+function computeAge(person) {
+    if (person.yearOfDeath === undefined) {
+        return new Date().getFullYear() - person.yearOfBirth;
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
