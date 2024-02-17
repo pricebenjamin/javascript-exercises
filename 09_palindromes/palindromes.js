3848f8f27d245eff8a987fd6df3cf06b3be5bb34
Ben Price <price.benjamindaniel@gmail.com>
2024-02-16 16:09:29 -0800

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const stripped = text.toLowerCase()
+        .split("")
+        .filter(char => isAlphanumeric(char));
+    const reversed = stripped.toReversed();
 
+    return stripped.join("") == reversed.join("");
 };
+
+function isAlphanumeric(char) {
+    return (
+        ("0" <= char && char <= "9") ||
+        ("A" <= char && char <= "Z") ||
+        ("a" <= char && char <= "z")
+    );
+}
 
 // Do not edit below this line
 module.exports = palindromes;
